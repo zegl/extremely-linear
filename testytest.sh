00000631184f9d1540c6e18e65bf5262c9692fab
Fee Gevaert <fee.gevaert@opengeohub.org>
2023-10-06 17:49:12 +0200

    linearize: fixed comment

--- testytest.sh
+++ testytest.sh
@@ -17,13 +17,19 @@ make_dummy_commit
 # a-b-e-f main
 #   \
 #    c-d new-branch
-run_git_linearize "-v --short"
+run_git_linearize "--short"
 # 0-1-2 main
 # a-c-d new-branch
 # git switch new-branch
 # run_git_linearize "-v --short"
 # now we have to git rebase --onto 0 a
 if [[ $(git merge-base main new-branch) == 000001* ]]; then
+  echo "yay!"
+else
+  echo "nay! $(git merge-base main new-branch)"
+fi
+
+if [[ $(git merge-base main branch-2) == 000002* ]]; then
   echo "yay!"
 else
   echo "nay! $(git merge-base main new-branch)"
